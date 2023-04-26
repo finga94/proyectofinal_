26da40e91b1016d5001d8ff4ed6fcae7b2c2cebd
USUARIO <USUARIO@FJIR-U1T290H>
2023-04-26 00:55:51 -0500

    Es la version de  poo v1.1

--- Documentos/Proyecto_2023_finga/src/ec/edu/intsuperior/vista/Aplicacion.java
+++ Documentos/Proyecto_2023_finga/src/ec/edu/intsuperior/vista/Aplicacion.java
@@ -5,10 +5,55 @@
  */
 package ec.edu.intsuperior.vista;
 
+import java.util.Scanner;
+
 /**
  *
  * @author USUARIO
  */
 public class Aplicacion {
+     static Scanner leer = new Scanner(System.in);
     
+    public static void main(String[] args) {
+               Aplicacion.adivinarNumero();
+        
+    }
+    
+    public static void metodo1(){
+        int i=1;
+        while(i<=5){
+            System.out.println(i);
+            i++;
+        }
+        
+    }
+    
+    public static void metodo2(){
+        
+        for(int i=1;i<=5;i++){
+            System.out.println(i);
+        }        
+    }
+    
+    public static void metodo3(){
+        int i=1;
+        do{
+            System.out.println(i);
+            i++;
+        }while(i<=5);
+    }
+    
+    public static void adivinarNumero(){
+        int n=(int)(Math.random()*10);
+        int adivina;
+        do{
+        System.out.println(n+"ingresa un numero para ver si adivinas");
+         adivina=leer.nextInt();
+        if (adivina==n) {
+            System.out.println("Felicidades ganaste");
+        } else {
+            System.out.println("Intenta de nuevo");
+        }
+        }while(adivina!=n);
+    }
 }
